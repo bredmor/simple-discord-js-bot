2333eb7bae1db847b9e24b27f96eaaef7090c6b6
D99-1 <79247409+D99-1@users.noreply.github.com>
2022-08-10 17:47:06 +1000

    Update talk.js

--- handlers/commands/talk.js
+++ handlers/commands/talk.js
@@ -2,11 +2,14 @@
  * This class responds to anyone that types !bot talk and chooses one of the phrases below to respond with at random.
  *
  */
+user = message.mentions.users.first();
 module.exports = {
+    
     name: 'talk', // The name of the command
     description: 'Random phrases', // The description of the command (for help text)
     args: false, // Specified that this command doesn't need any data other than the command
     usage: '', // Help text to explain how to use the command (if it had any arguments)
+    cooldown:100
     execute(message, args) {
 
         // List of phrases to respond with
@@ -16,6 +19,6 @@ module.exports = {
             'I am the very model of a modern major general.'
         ];
 
-        return message.reply(phrases[Math.floor(Math.random()*phrases.length)]); // Replies to the user with a random phrase
+        return message.reply('Hello'+ user);\
     },
 };
